92d6f04ada885bce13d54ebf6905014173fa1be6
Lily Yao <lilyyao102@gmail.com>
2022-02-22 21:06:47 -0700

    Initial Commit for REST API for Goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,10 +1,13 @@
 const asyncHandler = require('express-async-handler')
 
+const Goal = require('../models/goalsModel')
+
 //@desc Get goals
 //@route GET /api/goals
 //@access private
 const getGoals = asyncHandler( async (req,res) => {
-    res.status(200).json({message: 'Get Goals'})
+    const goals = await Goal.find()
+    res.status(200).json(goals)
 })
 
 //@desc Set goals
@@ -15,7 +18,12 @@ const setGoals = asyncHandler( async (req,res) => {
         res.status(400)
         throw new Error('Please add a text field')
     }
-    res.status(200).json({message: 'Set Goals'})
+
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+
+    res.status(200).json(goal)
 })
 
 //@desc Update goals
@@ -23,14 +31,35 @@ const setGoals = asyncHandler( async (req,res) => {
 //@access private
 //back ticks to include a variable
 const updateGoals = asyncHandler( async (req,res) => {
-    res.status(200).json({message: `Update Goal ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    })
+
+    res.status(200).json(updatedGoal)
 })
 
 //@desc Delete goals
 //@route DELETE /api/goals/id
 //@access private
 const deleteGoals = asyncHandler( async (req,res) => {
-    res.status(200).json({message: `Delete Goal ${req.params.id}`})
+
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    await goal.remove()
+
+    res.status(200).json({id: req.params.id})
 })
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config() //env lets us customize individual working environment variables
 const {errorHandler} = require('../backend/middleware/errorMiddleware')
+const connectDB = require('../backend/config/db')
 const port = process.env.PORT || 3000 //access PORT from .env or use 5000 if port not found
+
+connectDB()
 
 const app = express() //initialize express
 
