ec3d3f705ba4c93555e90cee2eb18a44b10a475c
Lily Yao <lilyyao102@gmail.com>
2022-02-22 20:16:19 -0700

    added controllers and middleware

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,19 @@
+//middleware = functions that execute during req res cycle
+
+//going to oerwite express default error handler
+const errorHandler = (err, req, res, next) => {
+
+    //ternary conditional, if res has status code use it otherwise set statusCode to 500
+    const statusCode = res.statusCode ? res.statusCode : 500
+
+    res.status(statusCode)
+
+    res.json({
+        message: err.message,
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack
+    })
+}
+
+module.exports = {
+    errorHandler
+}
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log('Hello world')
+const express = require('express')
+const dotenv = require('dotenv').config() //env lets us customize individual working environment variables
+const {errorHandler} = require('../backend/middleware/errorMiddleware')
+const port = process.env.PORT || 3000 //access PORT from .env or use 5000 if port not found
+
+const app = express() //initialize express
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false}))
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
