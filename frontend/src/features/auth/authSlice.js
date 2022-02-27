375457a5550f19c26d01eafdc7e10b8e3722eae8
Lily Yao <lilyyao102@gmail.com>
2022-02-26 17:16:35 -0700

    register working

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,4 +1,5 @@
  import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+ import authService from './authService'
 
  //Get user from local storage
  const user= JSON.parse(localStorage.getItem('user'))
@@ -14,9 +15,12 @@
  //Register user
  export const register = createAsyncThunk('auth/register', async (user, thunkAPI) => {
      try {
-         
+         return await authService.register(user)
      } catch (error) {
-         
+         const message = (error.response && error.response.data && error.response.data.message) 
+         || error.message || error.toString()
+         //thunkAPI has method rejectw/value to return message as payload
+         return thunkAPI.rejectWithValue(message)
      }
  })
 
@@ -31,7 +35,23 @@
             state.message = ''
         }
      },
-     extraReducers: () => {}
+     extraReducers: (builder) => {
+         builder
+            .addCase(register.pending, (state => {
+                state.isLoading = true
+            }))
+            .addCase(register.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.isSuccess = true
+                state.user = action.payload
+            })
+            .addCase(register.rejected, (state, action) => {
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+                state.user = null
+            })
+     }
  })
 
 export const {reset} = authSlice.actions
